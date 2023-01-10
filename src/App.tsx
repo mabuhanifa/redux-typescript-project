532d9342104b0e46563e021164115455450b2da1
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-10 23:52:36 +0600

    tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux/es/hooks/useDispatch';
 import './App.css';
+import Cart from './components/Cart';
 import { useAppSelector } from './hooks/useTypedSelectors';
 import { addToCart, fetchProducts, ProductType } from './redux/features/storeSlice';
 import { AppDispatch } from './redux/store/store';
@@ -16,16 +17,19 @@ function App() {
     dispatch(addToCart(product))
   }
   return (
-    <div>
-      <div >
+    <div className='grid grid-cols-3'>
+      <div className='grid grid-cols-3 gap-5'>
         {status === "pending" && <h1>loading.....</h1>}
         {products && products.map(product => (
-          <div key={product.id}>
+          <div key={product.id} className="border border-gray-300">
             <h2>{product.title}</h2>
             <button onClick={() => fnAddToCart(product)}>Add to Cart</button>
           </div>
         ))
         }
+      </div>
+      <div>
+        <Cart />
       </div>
     </div>
   );
