dce7bcf8d0a9ceeee3a48b589a50cbefd3267ffa
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-11 03:14:32 +0600

    btn styles

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -6,16 +6,21 @@ export default function Cart() {
         <div className="flex flex-col gap-2">
             {
                 cart && cart.map((c) => (
-                    <div className="p-5 border border-gray-300 flex gap-5" key={c.id}>
-                        <div className="flex justify-center items-center"><img src={c.thumbnail} alt="" className="h-14 w-20 object-cover" /></div>
-                        <div>
+                    <div className="p-2 border border-gray-300 gap-3 grid grid-cols-5" key={c.id}>
+                        <div className="flex justify-center items-center">
+                            <img src={c.thumbnail} alt="" className="h-14 w-20 object-cover" />
+                        </div>
+                        <div className="col-span-2">
                             <h1>{c.title}</h1>
                             <h2>Price per unit : $ {c.price}</h2>
                             <h2>Total Price : $ {c.quantity ? c.quantity * c.price! : 1 * c.price!}</h2>
                         </div>
-                        <div className="flex flex-col">
-                            <button>+</button>
-                            <button>-</button>
+                        <div className="flex flex-col items-center justify-center gap-3 font-bold text-white text-xl">
+                            <button className="rounded px-2.5 bg-green-500">+</button>
+                            <button className="rounded px-3 bg-red-500 ">-</button>
+                        </div>
+                        <div className="flex items-center justify-center">
+                            <button className="bg-red-500 w-full py-2  text-white">REMOVE</button>
                         </div>
                     </div>
                 ))
