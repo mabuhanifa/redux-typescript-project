474fa2347c9e406f4c7a39ad05663098d3102350
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-11 00:05:11 +0600

    img

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,18 @@ function App() {
     dispatch(addToCart(product))
   }
   return (
-    <div className='grid grid-cols-3'>
-      <div className='grid grid-cols-3 gap-5'>
+    <div className='p-10 grid grid-cols-3'>
+      <div className='col-span-2 grid grid-cols-3 gap-2'>
         {status === "pending" && <h1>loading.....</h1>}
         {products && products.map(product => (
-          <div key={product.id} className="border border-gray-300">
+          <div key={product.id} className="border border-gray-300 p-3 rounded shadow-lg">
+            <img src={product.thumbnail} alt="" className='h-32 w-60 object-cover' />
             <h2>{product.title}</h2>
-            <button onClick={() => fnAddToCart(product)}>Add to Cart</button>
+            <div className='flex justify-center'>
+              <button onClick={() => fnAddToCart(product)}
+                className='w-full py-2 bg-indigo-700 text-white rounded'
+              >Add to Cart</button>
+            </div>
           </div>
         ))
         }
