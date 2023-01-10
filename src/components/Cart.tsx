9fece65e704e2460a726c2e5bc1480c57909ecd2
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-11 05:43:55 +0600

    totalPrice

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -3,8 +3,10 @@ import CartItem from "./CartItem";
 
 export default function Cart() {
     const { store: { cart } } = useAppSelector((state) => state);
+    const totalPrice = cart.reduce((a, c) => a + ((c.price ? c.price : 1) * (c.quantity ? c.quantity : 1)), 0)
     return (
         <div className="flex flex-col gap-2">
+            {totalPrice}
             {
                 cart && cart.map((cart) => <CartItem cart={cart} key={cart.id} />
                 )
--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -10,37 +10,42 @@ export default function CartItem(props: PropsType) {
     const { cart } = props;
     const dispatch = useDispatch<AppDispatch>();
     const changeQty = (id: number, qty: number) => {
-        console.log(id, qty);
         dispatch(changeQuantity({
             id: id,
             quantity: qty
         }))
     }
     return (
-        <div className="p-2 border border-gray-300 gap-3 grid grid-cols-5" key={cart.id}>
-            <div className="flex justify-center items-center">
-                <img src={cart.thumbnail} alt="" className="h-14 w-20 object-cover" />
-            </div>
-            <div className="col-span-2">
-                <h1>{cart.title}</h1>
-                <h2>Price per unit : $ {cart.price}</h2>
-                <h2>Selected Quantity : {cart.quantity ? cart.quantity : 1}</h2>
-                <h2>Total Price : $ {cart.quantity ? cart.quantity * cart.price! : 1 * cart.price!}</h2>
-            </div>
-            <div className="flex flex-col items-center justify-center gap-3 font-bold text-white text-xl">
-                <button className="rounded px-2.5 bg-green-500"
-                    onClick={() => changeQty(cart.id, cart.quantity ? cart.quantity + 1 : 2)}
+        <div>
+            <div className='text-center'>
 
-                >+</button>
-                <button className="rounded px-3 bg-red-500 "
 
-                    onClick={() => changeQty(cart.id, cart.quantity ? cart.quantity - 1 : 0)}
-                >-</button>
             </div>
-            <div className="flex items-center justify-center">
-                <button className="bg-red-500 w-full py-2  text-white"
-                    onClick={() => dispatch(removeFromCart({ id: cart.id }))}
-                >REMOVE</button>
+            <div className="p-2 border border-gray-300 gap-3 grid grid-cols-5" key={cart.id}>
+                <div className="flex justify-center items-center">
+                    <img src={cart.thumbnail} alt="" className="h-14 w-20 object-cover" />
+                </div>
+                <div className="col-span-2">
+                    <h1>{cart.title}</h1>
+                    <h2>Price per unit : $ {cart.price}</h2>
+                    <h2>Selected Quantity : {cart.quantity ? cart.quantity : 1}</h2>
+                    <h2>Total Price : $ {cart.quantity ? cart.quantity * cart.price! : 1 * cart.price!}</h2>
+                </div>
+                <div className="flex flex-col items-center justify-center gap-3 font-bold text-white text-xl">
+                    <button className="rounded px-2.5 bg-green-500"
+                        onClick={() => changeQty(cart.id, cart.quantity ? cart.quantity + 1 : 2)}
+
+                    >+</button>
+                    <button className="rounded px-3 bg-red-500 "
+
+                        onClick={() => changeQty(cart.id, cart.quantity ? cart.quantity - 1 : 0)}
+                    >-</button>
+                </div>
+                <div className="flex items-center justify-center">
+                    <button className="bg-red-500 w-full py-2  text-white"
+                        onClick={() => dispatch(removeFromCart({ id: cart.id }))}
+                    >REMOVE</button>
+                </div>
             </div>
         </div>
     )
