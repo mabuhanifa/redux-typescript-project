b77b0c321bc4e1d4ceafd9dca4edbd78720c4402
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-10 21:37:20 +0600

    store

--- src/redux/features/storeSlice.tsx
+++ src/redux/features/storeSlice.tsx
@@ -1,14 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit/dist/createAsyncThunk";
+
+interface MainState {
+    products: [];
+    cart: [];
+    loading: 'idle' | 'pending' | 'succeeded' | 'failed';
+}
+
 
 const initialState = {
     products: [],
     cart: [],
+    loading: 'idle',
 }
 
+const fetchProducts = createAsyncThunk(
+    'store/products',
+    async () => {
+        const response = await fetch(``)
+        return (await response.json())
+    }
+)
+
 const storeSlice = createSlice({
-    name: 'products',
+    name: 'store',
     initialState,
-    reducers: {
+    reducers: {},
+    extraReducers: (builder) => {
 
     }
 })
---
+++ src/redux/store/store.tsx
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import storeReducer from '../features/storeSlice';
+// ...
+const store = configureStore({
+    reducer: {
+        store: storeReducer
+    },
+})
+export type RootState = ReturnType<typeof store.getState>
+
+export default store;
