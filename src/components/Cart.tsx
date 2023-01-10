6d5016c16c0e0d0ee40b9a09a3e604ee4ad39a07
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-11 02:54:12 +0600

    cart styles

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -3,13 +3,20 @@ import { useAppSelector } from "../hooks/useTypedSelectors";
 export default function Cart() {
     const { store: { cart } } = useAppSelector((state) => state);
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-2">
             {
                 cart && cart.map((c) => (
-                    <div className="p-5" key={c.id}>
-                        <h1>{c.title}</h1>
-                        <h2>Price per unit : {c.price}</h2>
-                        <h2>Total Price {c.quantity ? c.quantity * c.price! : 1 * c.price!}</h2>
+                    <div className="p-5 border border-gray-300 flex gap-5" key={c.id}>
+                        <div className="flex justify-center items-center"><img src={c.thumbnail} alt="" className="h-14 w-20 object-cover" /></div>
+                        <div>
+                            <h1>{c.title}</h1>
+                            <h2>Price per unit : $ {c.price}</h2>
+                            <h2>Total Price : $ {c.quantity ? c.quantity * c.price! : 1 * c.price!}</h2>
+                        </div>
+                        <div className="flex flex-col">
+                            <button>+</button>
+                            <button>-</button>
+                        </div>
                     </div>
                 ))
             }
