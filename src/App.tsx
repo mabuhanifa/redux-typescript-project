5c40f87b6af55bcf055c4608f6a49686d096da66
shourovahmd <moh.abuhanifa@gmail.com>
2023-01-20 11:51:14 +0600

    title

--- src/App.tsx
+++ src/App.tsx
@@ -12,48 +12,51 @@ function App() {
   }, [dispatch]);
 
   return (
-    <div className='p-5 grid grid-cols-2 md:grid-cols-3'>
-      <div className='col-span-1 md:col-span-2 grid md:grid-cols-3 gap-3 p-2'>
+    <>
+      <h1 className='text-center text-2xl font-bold mt-5 text-indigo-700'>Redux-Typescript</h1>
+      <div className='p-5 grid grid-cols-2 md:grid-cols-3'>
+        <div className='col-span-1 md:col-span-2 grid md:grid-cols-3 gap-3 p-2'>
 
-        {status === "pending" && <h1>loading.....</h1>}
+          {status === "pending" && <h1>loading.....</h1>}
 
-        {products && products.map(product => (
+          {products && products.map(product => (
 
-          <div key={product.id} className="border border-gray-300 p-3 rounded shadow-lg  relative h-[360px]">
+            <div key={product.id} className="border border-gray-300 p-3 rounded shadow-lg  relative h-[360px]">
 
-            <img src={product.thumbnail} alt="" className='h-32 w-60 object-cover' />
+              <img src={product.thumbnail} alt="" className='h-32 w-60 object-cover' />
 
-            <h2 className='text-lg font-bold my-3'>{product.title}</h2>
-            <p>
-              {product.description}
-            </p>
-            <h4 className='font-bold'>Price : {product.price}</h4>
-            <div className='flex justify-center absolute bottom-2 left-2 right-2'>
+              <h2 className='text-lg font-bold my-3'>{product.title}</h2>
+              <p>
+                {product.description}
+              </p>
+              <h4 className='font-bold'>Price : {product.price}</h4>
+              <div className='flex justify-center absolute bottom-2 left-2 right-2'>
 
-              {
-                cart.some((c) => c.id === product.id) ?
+                {
+                  cart.some((c) => c.id === product.id) ?
 
-                  <button className="bg-red-500 w-full py-2  text-white rounded"
-                    onClick={() => dispatch(removeFromCart({ id: product.id }))}
-                  >REMOVE</button>
+                    <button className="bg-red-500 w-full py-2  text-white rounded"
+                      onClick={() => dispatch(removeFromCart({ id: product.id }))}
+                    >REMOVE</button>
 
-                  :
+                    :
 
-                  <button onClick={() => dispatch(addToCart(product))}
-                    className='w-full py-2 bg-indigo-700 text-white rounded'
-                  >Add to Cart</button>
+                    <button onClick={() => dispatch(addToCart(product))}
+                      className='w-full py-2 bg-indigo-700 text-white rounded'
+                    >Add to Cart</button>
 
-              }
+                }
 
+              </div>
             </div>
-          </div>
-        ))
-        }
+          ))
+          }
+        </div>
+        <div className='p-2 col-span-1 '>
+          <Cart />
+        </div>
       </div>
-      <div className='p-2 col-span-1 '>
-        <Cart />
-      </div>
-    </div>
+    </>
   );
 }
 
